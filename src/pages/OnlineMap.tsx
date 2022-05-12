40cd206381048ab211bd9b1bbaa90bf43e6c5caa
Woojung0618 <woojung0618@gmail.com>
2022-05-13 02:41:59 +0900

    TEST: OnlineMap Animation Test

--- src/pages/OnlineMap.tsx
+++ src/pages/OnlineMap.tsx
@@ -1,9 +1,41 @@
-import { motion } from "framer-motion"
+import react, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import { Colors } from '../styles/ui';
+import { motion, AnimatePresence } from "framer-motion";
+
+import MapImg from '../assets/MapTest.svg';
+import { Motion } from 'react-motion';
 
 const OnlineMap = () => {
+    const unit = 100;
+    const [dir, setDir] = useState(true);
+
+
+    const mapVariants = {
+        entry: ({
+            x: 0
+        }),
+        center:{
+            x: dir ? (-1 * unit): (unit)
+        },
+        exit: ({
+            x: 0
+        }),
+
+    }
+
     return (
-        <div>OnlineMap
-            <motion.div className="box" />
+        <div>
+            <button onClick={() => setDir(!dir)}>방향전환</button>
+            <AnimatePresence>
+                    <motion.img
+                        variants={mapVariants}
+                        src={MapImg}
+                        initial="entry"
+                        animate="center"
+                        exit="exit"
+                    />
+            </AnimatePresence>
         </div>
     );
 };
