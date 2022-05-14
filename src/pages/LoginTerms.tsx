6037209d238215b05068dd48706092c155d81e0d
leeyeonghyeon <yeonghyeon0403@naver.com>
2022-05-15 03:43:01 +0900

    FEAT: 오프라인지도-아이템 정렬, 모달 일부 구현

---
+++ src/pages/LoginTerms.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const LoginTerms = () => {
+    return (
+        <>
+        <div>Login Terms page</div>
+        </>
+    );
+};
+
+export default LoginTerms;
