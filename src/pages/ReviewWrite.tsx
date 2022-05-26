16196b41b091a844d1c4b9df91213b363e39d88f
leeyeonghyeon <yeonghyeon0403@naver.com>
2022-05-26 18:27:51 +0900

    FEAT: 오프라인지도-리뷰 화면 구성

--- src/pages/ReviewWrite.tsx
+++ src/pages/ReviewWrite.tsx
@@ -1,5 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styled from '@emotion/styled';
+import { Colors } from '../styles/ui';
+
+import { ReactComponent as StarAvg } from "../assets/offlineMap/StarAvg.svg"
 
 interface Props {
     state: {
@@ -20,13 +24,61 @@ const ReviewWrite = () => {
     console.log('리뷰: '+ location.state );
     
     return (
-        <div>
-            {location.state.name}
+        <div style={{height: 'calc(100vh - 70px)', display: 'flex', alignItems: 'flex-end', justifyContent: 'center'}}>
+            <Section>
+            <Store>
+                <div style={{float: 'left', width: '90%'}}>
+                    <span className='storeName' style={{float: 'left'}}>{location.state.name}</span>
+                    <span className='storeCategory' style={{float: 'left'}}>{location.state.category}</span>
+                </div>
+                <div className='storeReview' style={{float: 'right', width: '10%'}}>
+                    <StarAvg style={{float: 'left'}}/>
+                    <span style={{float: 'left'}}>&nbsp;{location.state.starAvg}/5</span>
+                    <span style={{float: 'left'}}>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+                    <span style={{float: 'left'}}>리뷰 {location.state.reviewCnt}</span>
+                </div>
+            </Store>
         
 
             
+            </Section>
         </div>
     );
 };
 
 export default ReviewWrite;
+
+const Section = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 75vw;
+    height: calc(96vh - 112px);
+
+    padding: 42px 144px 0px 144px;
+
+    background: ${Colors.white};
+`;
+
+const Store = styled.div`
+    display: flex;
+    flex-direction: row;
+    
+    font-family: 'NotoSans';
+    font-weight: 500;
+    color: ${Colors.black};
+
+    .storeName{
+        font-size: 25px;
+    }
+
+    .storeCategory{
+        margin-left: 8px;
+        font-size: 11px;
+        color: ${Colors.gray2};
+    }
+
+    .storeReview{
+        font-size: 14px;
+    }
+`;
